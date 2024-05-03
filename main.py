4f75b712d37f7e287a8fd5613a60a2f934e9239b
agafinaa <135313062+agafinaa@users.noreply.github.com>
2024-05-03 10:55:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,46 @@ import numpy as np
 import pytesseract
 from PIL import Image
 from pix2tex.cli import LatexOCR
-from urllib.parse import quote
 import fitz
+import requests
+import base64
 
-# import magic
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 config = r'--oem 3 --psm 6'
+
+
+def auto_rotate(image_path):
+    img = cv2.imread(image_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # обнаруживаем края на изображении и ищем прямые линии
+    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+
+    if lines is not None:
+        angles = []
+        for rho, theta in lines[:, 0]:
+            angle = (theta * 180 / np.pi - 90)
+            angles.append(angle)
+
+        # вычисление среднего угла
+        median_angle = np.median([angle for angle in angles if angle != 0])
+        # поворот
+        (h, w) = img.shape[:2]
+        center = (w // 2, h // 2)
+        m = cv2.getRotationMatrix2D(center, median_angle, 1.0)
+        rotated = cv2.warpAffine(img, m, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+        return rotated
+    else:
+        return img
+
+
+def query(q):
+    API_URL = "https://api-inference.huggingface.co/models/Norm/nougat-latex-base"
+    headers = {"Authorization": f"Bearer {'ЗАГРУЗИ СВОЙ ТОКЕН'}"}
+    response = requests.request("POST", API_URL, headers=headers, json=q)
+    return response.json()
 
 
 # Это один раз надо запустить перед работой
@@ -60,11 +94,14 @@ def start(file_path):
                     "\\begin{document}\n")
 
         for i in range(page_count):
-            create_tex(f"page_{i + 1}.png", i)
+            file_page_path = f"page_{i + 1}.png"
+            rotated_image = auto_rotate(file_page_path)
+            cv2.imwrite(file_page_path, rotated_image)
+            # create_tex(f"page_{i + 1}.png", i)
+            create_tex(file_page_path, i)
 
         with open("output.tex", "a", encoding="utf-8") as f:
             f.write("\\end{document}")
-
 
     elif file_path.lower().endswith('.png') or file_path.lower().endswith('.jpg') or file_path.lower().endswith('.jpeg'):
         with open("output.tex", "w", encoding="utf-8") as f:
@@ -77,15 +114,19 @@ def start(file_path):
                     "\\usepackage{dsfont}\n"
                     "\\usepackage{graphicx}\n\n"
                     "\\begin{document}\n")
+
+        # пытаемся перевернуть каждое изображение
+        rotated_image = auto_rotate(file_path)
+        cv2.imwrite(file_path, rotated_image)
         create_tex(file_path, 0)
         with open("output.tex", "a", encoding="utf-8") as f:
             f.write("\\end{document}")
 
 
-def create_tex(img_path, numper_of_page):
+def create_tex(img_path, number_of_page):
     img1 = cv2.imread(img_path)
     height, width, _ = img1.shape
-    results = best_model(img1, imgsz=640, iou=0.4, conf=0.4, verbose=True)
+    results = best_model(img1, imgsz=640, iou=0.8, conf=0.3, verbose=True)
     img = cv2.cvtColor(img1, cv2.COLOR_RGB2BGR)
 
     # получение классов и имен классов
@@ -98,19 +139,70 @@ def create_tex(img_path, numper_of_page):
     # Создаем список индексов, который будет содержать индексы отсортированных объектов
     indexes = list(range(len(boxes)))
 
+    diff = 3000
+    for i in range(len(indexes)):
+        if classes[i] != 2:
+            diff = min(diff, boxes[i][3] - boxes[i][1])
+
     # сортировка на основе сначала среднего y, затем среднего x
     # ..// 10 - учёт погрешности, чтобы небольшая разница в y не позволяла считать блоки разными строками
-    indexes.sort(key=lambda j: ((boxes[j][1] + boxes[j][3]) / 2 // 10, (boxes[j][0] + boxes[j][2]) / 2))
+    indexes.sort(key=lambda u: ((boxes[u][1] + boxes[u][3]) / 2 // diff, (boxes[u][0] + boxes[u][2]) / 2))
 
     len_indexes = len(indexes)
 
     caption = False
     prev_text = ''  # для добавления пропащих точек и пробелов
 
-    with open("output.tex", "a", encoding="utf-8") as f:
+    with (open("output.tex", "a", encoding="utf-8") as f):
         # переход на новую страницу
-        if numper_of_page != 0:
+        if number_of_page != 0:
             f.write('\n\\newpage\n')
+
+        hight_of_line = 1000
+        left_x_line = 1000
+
+        
+        # удаление дубляжа сегментов, внутренних повторяющихся сегментов
+        for i in range(len_indexes):
+            box = boxes[indexes[i]]
+            x_min_i, y_min_i, x_max_i, y_max_i = map(int, box)
+            hight_of_line = min(hight_of_line, y_max_i - y_min_i)
+            left_x_line = min(left_x_line, x_min_i)
+
+            '''cropped_image_i = img[y_min_i:y_max_i, x_min_i:x_max_i]
+            cropped_image_i = Image.fromarray(cropped_image_i)
+            cropped_image_i.save("cropped_image_i.png")'''
+            for j in range(i + 1, len_indexes):
+                box = boxes[indexes[j]]
+                x_min_j, y_min_j, x_max_j, y_max_j = map(int, box)
+
+                '''cropped_image_j = img[y_min_j:y_max_j, x_min_j:x_max_j]
+                cropped_image_j = Image.fromarray(cropped_image_j)
+                cropped_image_j.save("cropped_image_j.png")'''
+
+                if x_min_j >= x_min_i and y_min_j >= y_min_i and x_max_j <= x_max_i and y_max_j <= y_max_i:
+                    classes[indexes[j]] = 4  # ничего не делаем с дубляжом, пропускаем далее
+                elif x_min_j <= x_min_i and y_min_j <= y_min_i and x_max_j >= x_max_i and y_max_j >= y_max_i:
+                    classes[indexes[i]] = 4  # ничего не делаем с дубляжом, пропускаем далее
+                elif x_max_j - x_min_j >= x_max_i - x_min_i and y_max_j - y_min_j <= y_max_i - y_min_i \
+                        and abs(y_max_j - y_min_j - (y_max_i - y_min_i)) <= hight_of_line / 4 \
+                        and y_min_i <= ((y_max_j + y_min_j) / 2) <= y_max_i \
+                        and x_min_i <= ((x_max_j + x_min_j) / 2) <= x_max_i:
+                    classes[indexes[j]] = 4
+                elif x_max_j - x_min_j <= x_max_i - x_min_i and y_max_j - y_min_j >= y_max_i - y_min_i \
+                        and y_min_j <= ((y_max_i + y_min_i) / 2) <= y_max_j \
+                        and x_min_j <= ((x_max_i + x_min_i) / 2) <= x_max_j:
+                    classes[indexes[i]] = 4
+
+        # тест
+        '''for i in range(len_indexes):
+            if classes[indexes[i]] != 4:
+                box = boxes[indexes[i]]
+                x_min_i, y_min_i, x_max_i, y_max_i = map(int, box)
+                cropped_image_i = img[y_min_i:y_max_i, x_min_i:x_max_i]
+                cropped_image_i = Image.fromarray(cropped_image_i)
+                cropped_image_i.save(f"crop_{i}.png", format='PNG')
+                print(i, ': ', x_min_i, y_min_i, x_max_i, y_max_i)'''
 
         for i in range(len_indexes):
             # если мы уже брали сегмент как подпись, то скип
@@ -118,7 +210,7 @@ def create_tex(img_path, numper_of_page):
                 caption = False
                 continue
             box = boxes[indexes[i]]
-            x_min, y_min, x_max, y_max = map(int, box[:4])
+            x_min, y_min, x_max, y_max = map(int, box)
             # print('x: ', (x_min + x_max) / 2, 'y: ', (y_min + y_max) / 2)
             cropped_image = img[y_min:y_max, x_min:x_max]
             cropped_image = Image.fromarray(cropped_image)
@@ -128,18 +220,58 @@ def create_tex(img_path, numper_of_page):
             # проверка на перенос строки
             if i != 0:
                 box_prev = boxes[indexes[i - 1]]
-                x_min_prev, y_min_prev, x_max_prev, y_max_prev = map(int, box_prev[:4])
+                x_min_prev, y_min_prev, x_max_prev, y_max_prev = map(int, box_prev)
                 # print('y_prev: ', (y_min_prev + y_max_prev) / 2)
-                if abs((y_min + y_max) / 2 - (y_min_prev + y_max_prev) / 2) >= 10:
+                # if abs((y_min + y_max) / 2 - (y_min_prev + y_max_prev) / 2) >= 10:
+                if abs((y_min + y_max) / 2 - (y_min_prev + y_max_prev) / 2) >= hight_of_line:
                     new_string = True
 
             if int(classes[indexes[i]]) == 0:
-                formula = '$' + model_latex(cropped_image) + '$'
+                # if new_string and prev_text.endswith('$'):
+                if new_string:
+                    f.write('\n\n')
+
+                flag_error = False
+                formula_nuga = ''
+                formula_latex = ''
+                try:
+                    with io.BytesIO() as output:
+                        cropped_image.save(output, format="PNG")
+                        image_bytes = output.getvalue()
+
+                    # это для использования с нугой
+                    image_b64 = base64.b64encode(image_bytes)
+                    formula = query({"inputs": image_b64.decode("utf-8"), "parameters": {"max_new_tokens": 800}})
+                    # print(formula)
+                    # print(type(formula))
+                    # print(type(formula[0]))
+                    # print(type(formula[0]['generated_text']))
+
+                    formula_nuga = '$' + formula[0]['generated_text'] + '$'
+                except Exception as e:
+                    flag_error = True
+                    formula_latex = '$' + model_latex(cropped_image) + '$'
+                else:
+                    formula_latex = '$' + model_latex(cropped_image) + '$'
+
+                if flag_error:
+                    formula = formula_latex
+                elif 0 < len(formula_latex) < len(formula_nuga):
+                    formula = formula_latex
+                else:
+                    formula = formula_nuga
+
+                # это для использования с latex-ocr
+                """formula = '$' + model_latex(cropped_image) + '$'"""
 
                 if i != 0 and new_string is False:
                     if prev_text[-1] != ' ':
                         f.write(' ')
 
+                # блок использовался для latex-ocr
+                # f.write(formula)
+
+                # formula = '$' + formula[0]['generated_text'] + '$'
                 f.write(formula)
                 f.write(' ')
                 prev_text = formula
@@ -147,25 +279,34 @@ def create_tex(img_path, numper_of_page):
                 text = pytesseract.image_to_string(cropped_image, config=config, lang='rus+eng')
                 if text == '':
                     continue
-                # исправление странного глюка pytesseract'a. Удаление _ вначале
-                elif text[0] == '_':
-                    text = text[1:]
                 # пустые символы мешают понять новый абзац или нет
-                while text[0] == ' ' or text[0] == '`' or text[0] == '\"' or text[0] == "\'" or text[0] == '‘':
+                while text[0] == ' ' or text[0] == '`' or text[0] == '\"' or text[0] == "\'" or text[0] == '‘' or text[0] == '_':
                     text = text[1:]
                 # удаление переносов
-                if text.endswith('-\n'): # 'это быстрее, но не обрабатывает внутри больших абзацев
+                prev_k = 0
+                flag_delete_perenosy = 0
+                text1 = text
+                text = ''
+                for k in range(len(text1) - 2):
+                    if text1[k:k + 2] == '-\n': # or text1[k:k + 1] == '\n':
+                        # f.write(text1[prev_k:k])
+                        text += text1[prev_k:k]
+                        prev_k = k + 2
+                        flag_delete_perenosy = 1
+                if flag_delete_perenosy == 1:
+                    # text = text[prev_k:len(text)]
+                    text += text1[prev_k:len(text1)]
+                else:
+                    text = text1
+                if text.endswith('-\n'): # переносы в конце абзацев или коротких кусков текста
                     text = text[:-2]
-                '''for j in range(1, len(text)):  # а это долго, но красивее
-                    if text[j - 1] == '-' and text[j] == '\n':
-                        text = text[:(j - 1)] + text ..... дописать, если надо'''
                 # считаем, что в большинстве случаев если текст начинается с большой буквы, то это новый абзац
                 # потому что если текст идёт после формулы, он начнётся с , или .
-                if text[0].isupper() and new_string:
-                    f.write('\n\n')
+                '''if text[0].isupper() and new_string:
+                    f.write('\n\n')'''
                 if new_string and i != 0 and y_max - y_min_prev >= 50: # пример: колонтитулы и текст далее не сливаются в один абзац
                     if text[0].islower():
-                        f.write('\n\\noindent\n')
+                        f.write('\n\n\\noindent\n')
                     else:
                         f.write('\n\n')
                 elif new_string and (text[0].isdigit() and text[1] == ')'
@@ -176,19 +317,12 @@ def create_tex(img_path, numper_of_page):
                                      or text[0].islower() and text[1].islower() and text[2] == ')'
                                      or text[0].islower() and text[1].islower() and text[2].islower() and text[3] == ')'):
                     f.write('\n\n')
-
-                ######## какая-то муть. надо переписать. неверно срабатывают условия
-                '''if i != 0 and new_string is False and text[0].isupper() and prev_text[-1] != ' ':
-                    f.write(' ')
-                elif i != 0 and (text[0] == ' ' or text[0] == ',' or text[0] == '.'):
-                    pass
-                elif i != 0:
-                    f.write(' ')'''
-                '''if i != 0 and new_string is False and (text[0] != ' ' or text[0] != ',' or text[0] != '.' or text[0].isupper()):
-                    if text[0].isupper():
-                        f.write('. ')
-                    if prev_text[-1] != ' ' and not (text[0] == ',' or text[0] == '.' or text[0] == ' '):
-                        f.write(' ')'''
+                elif new_string and left_x_line - 5 >= x_min >= left_x_line + 5:
+                    f.write('\n\n\\noident\n')
+                elif new_string and text[0].isupper():
+                    f.write('\n\n')
+                elif new_string and text[0].islower():
+                    f.write('\n\n\\noident\n')
 
                 # центрируем или нет
                 if abs(width/2 - (y_min + y_max)/2) <= 20 and text[0].isupper() and (new_string or i == 0) and (text[-1] == '.' or text[-1] == '\n') and len(text) < 50:
@@ -196,14 +330,14 @@ def create_tex(img_path, numper_of_page):
                     f.write(text)
                     f.write('\\end{center}')
                 else: # тут может быть i = 1, если впереди поймался колонтитул. надо это обработать????
-                    if text[0].islower() and (i == 0 or text[-2] == '\\' and text[-1] == 'n'):
-                        f.write('\\noindent\n')
+                    if text[0].islower() and i == 0 and text[-1] == '\n':
+                        f.write('\n\n\\noindent\n')
                     f.write(text)
                 prev_text = text
                 y_min_prev = y_min
-            else:
+            elif int(classes[indexes[i]] == 2):
                 i_safe = str(i)
-                str_number_of_page = str(numper_of_page)
+                str_number_of_page = str(number_of_page)
                 cropped_image.save('page' + str_number_of_page + '_' + i_safe + '.png', format='PNG')
 
                 if i == len_indexes - 1:
@@ -233,11 +367,11 @@ def create_tex(img_path, numper_of_page):
 
 
 model_latex = LatexOCR()
-best_model = YOLO('best.pt')
+best_model = YOLO('best15.04.pt')
 # это посмотреть метрики модели
 # metrics = best_model.val()
 # print(metrics)
 
-# test_visualization('photo_2024-03-27_16-33-01.jpg')
-start('photo_2024-03-27_16-33-01.jpg')
+# test_visualization('Михалёв - Начала алгебры,часть 1-092.png')
+start('Михалёв - Начала алгебры,часть 1-092.png')
 # test_visualization('page_2.png')
