e339e8abaa38390aac6929f2aff8b400521aeb8c
agafinaa <135313062+agafinaa@users.noreply.github.com>
2024-05-19 17:06:11 +0300

    Update test_visualization.py

--- test_visualization.py
+++ test_visualization.py
@@ -11,18 +11,19 @@ def test_visualization(image_path):
     img = cv2.cvtColor(img1, cv2.COLOR_RGB2BGR)
 
     # получение классов и имен классов
+    # вообще их 3, но вдруг в будущем будет больше
     classes = results[0].boxes.cls.cpu().numpy()
     class_names = results[0].names
 
-    # получение координат ограничивающих рамок объектов
+    # получение координат прямоугольников
     boxes = results[0].boxes.xyxy.cpu().numpy()
 
     # определение цветов для каждого класса
-    class_colors = {}
-    for i, class_index in enumerate(set(classes)):  # используем уникальные индексы классов
-        class_name = class_names[class_index]
-        color = tuple(np.random.randint(0, 256, 3).tolist())
-        class_colors[class_index] = color
+    class_colors = {
+        0: (255, 0, 0),   # синий для класса формул 0
+        1: (0, 0, 255),   # красный для класса текста 1
+        2: (0, 255, 255)  # желтый для класса картинок 2
+    }
 
     # создание изображения для отображения масок
     labeled_image = img.copy()
@@ -31,24 +32,19 @@ def test_visualization(image_path):
     for i, box in enumerate(boxes):
         x_min, y_min, x_max, y_max = map(int, box[:4])
         class_index = int(classes[i])
-        if class_index in class_colors:  # проверка, что класс имеет соответствующий цвет
-            color = class_colors[class_index]
-        else:
-            color = (0, 255, 0)  # используем зеленый цвет по умолчанию
+        color = class_colors.get(class_index, (0, 255, 0))  # использование зеленого по умолчанию на случай изменения структуры модели
 
         # прямоугольники и подписи
         cv2.rectangle(labeled_image, (x_min, y_min), (x_max, y_max), color, 2)
-        cv2.putText(labeled_image, class_names[class_index], (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color,
-                    2)
+        cv2.putText(labeled_image, class_names[class_index], (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
     # отображение результатов
     plt.figure(figsize=(8, 8), dpi=150)
     labeled_image = cv2.cvtColor(labeled_image, cv2.COLOR_BGR2RGB)
     plt.imshow(labeled_image)
     plt.axis('off')
-    plt.savefig('ВЫБЕРИТЕ ИМЯ ФАЙЛА.png', bbox_inches='tight', pad_inches=0)
+    plt.savefig('test(vers15.04.24).png', bbox_inches='tight', pad_inches=0)
     plt.show()
-
 
 # имя модели надо заменить на актуальное
 best_model = YOLO('best.pt')
